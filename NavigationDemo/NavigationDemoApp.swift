3c3e4157dff656e9768b7488a154c8df74c9de8d
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-15 12:37:38 +0100

    Initial Commit

---
+++ NavigationDemo/NavigationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationDemoApp.swift
+//  NavigationDemo
+//
+//  Created by Zaid Neurothrone on 2022-12-15.
+//
+
+import SwiftUI
+
+@main
+struct NavigationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
